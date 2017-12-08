6a70bf044381b02b781ed692d9646d682685958e
Valentin TOSI <valentin.tosi@gmail.com>
2017-12-08 15:54:07 +0100

    Test n°1.

---
+++ ile_interdite/src/ile_interdite/Test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ile_interdite;
+
+/**
+ *
+ * @author tosiv
+ */
+public class Test
+{
+    public Test()
+    {
+	System.out.println("Test");
+    }
+}
